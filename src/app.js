ff873fb69a4d85af25fbe74c9c2ba4fea3fd86f0
traian.dida <traian.dida@bonarea.com>
2022-05-03 18:00:49 +0200

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import config from './config'
 import routes from './routes/tree.routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(routes)
 app.set('port', config.port)
 
